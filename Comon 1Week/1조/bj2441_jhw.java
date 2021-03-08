ad5e4ca388a77b377f41ebc05dbf3c1fa57b4ea5
cousim46 <wjdghldns24@naver.com>
2021-03-08 19:01:45 +0900

    Update bj2441_jhw.java

--- Comon 1Week/1조/bj2441_jhw.java
+++ Comon 1Week/1조/bj2441_jhw.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class bj2441_jhw {
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
