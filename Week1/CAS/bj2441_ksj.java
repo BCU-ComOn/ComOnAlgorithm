e394be3970983eaae49b7f2a50fc8d898f0f7c90
cousim46 <wjdghldns24@naver.com>
2021-03-13 18:55:34 +0900

    CAS_feedBack

--- Week1/CAS/bj2441_ksj.java
+++ Week1/CAS/bj2441_ksj.java
@@ -13,7 +13,7 @@ public class bj2441_ksj {
             for (int j = len; j > i; j--) {
                 System.out.print("*");
             }
-
+            ////이부분에서는 그냥 System.out.println(); 이렇게만 해줘도 되요~
             System.out.println("");
         }
 
