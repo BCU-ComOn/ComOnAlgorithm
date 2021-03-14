a4c6129b9682e73fa1571ec59d2acf8fe3b10eb9
pocachp <pocachip0318@gmail.com>
2021-03-15 01:38:28 +0900

    Create bj2163_ksj.java

---
+++ Week1/CAS/bj2163_ksj.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class bj2163_ksj {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        System.out.println(M * N - 1);
+
+    }
+
+}
