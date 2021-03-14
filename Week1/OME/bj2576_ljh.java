6598f0f00f337bb9a2f849023ed5163906887e9a
jewhi <80098864+jewhi@users.noreply.github.com>
2021-03-15 02:08:44 +0900

    Update bj2576_ljh.java

--- Week1/OME/bj2576_ljh.java
+++ Week1/OME/bj2576_ljh.java
@@ -1,7 +1,4 @@
-import java.util.Scanner;
-
 public class bj2576_ljh {
-
 	public static void main(String[] args) {
 		int[] arr = new int[7];
 		int min = 100;
@@ -11,18 +8,19 @@ public class bj2576_ljh {
 		for(int i = 0; i < arr.length; i++) {
 			arr[i] = in.nextInt();
 			
-			if(min > arr[i]) 
-				min = arr[i];
-			
 			if(arr[i] % 2 != 0) {
 				sum += arr[i];
-			
+				
+			if(min > arr[i]) 
+				min = arr[i];
 			}
 		}
-			if(sum == 0) {
-				sum = -1;
-			}
+		if(sum == 0) {
+			System.out.println("-1");
+		}
+		else {
 			System.out.println(sum);
 			System.out.println(min);
-			}
+		}
 	}
+}
