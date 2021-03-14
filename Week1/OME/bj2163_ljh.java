9225c2aee335332f7573571d10da57d42cfedf6e
jewhi <80098864+jewhi@users.noreply.github.com>
2021-03-15 08:10:49 +0900

    Create bj2163_ljh.java

---
+++ Week1/OME/bj2163_ljh.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class bj2163_ljh {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int M = in.nextInt();
+		int N = in.nextInt();
+		
+		System.out.println(M * N -1);
+
+	}
+
+}
