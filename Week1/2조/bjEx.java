88d5a02f1523495b0863af1214451f20533a1151
LMY991 <dreamlmy1203@naver.com>
2021-03-15 23:10:00 +0900

    [bj2163_lmy]

--- Week1/2조/bjEx.java
+++ Week1/2조/bjEx.java
@@ -1,3 +1,16 @@
-public class bjEx {
-    
+import java.util.Scanner;
+
+public class bj2163_lmy {
+	public static void main(String[] args){
+    static int[][] resultArray = null;
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int M = Integer.parseInt(scan.nextLine().trim());
+        
+        System.out.println((M*N)-1);
+        
+        scan.close();
+    }
+}
 }
