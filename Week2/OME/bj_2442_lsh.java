c29c44ed81012aa729ead2fa073cb05ccb978f1d
LeeSeunghyeon-1 <ujm0524@naver.com>
2021-03-21 00:19:31 +0900

    bj_2442_lsh

---
+++ Week2/OME/bj_2442_lsh.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class bj2442_lsh {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for (int i = 0; i < num; i++) {
+
+            for (int j = i; j < num - 1; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+}
