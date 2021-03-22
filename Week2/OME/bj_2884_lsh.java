547b11273aad51543cf5554b1d7cc4b59c7fc1f4
LeeSeunghyeon-1 <ujm0524@naver.com>
2021-03-22 13:13:56 +0900

    bj_2884_lsh.java

---
+++ Week2/OME/bj_2884_lsh.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class bj_2884_lsh {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int H = sc.nextInt();
+        int M = sc.nextInt();
+
+        M -= 45;
+
+        if (M < 0) {
+            M += 60;
+            H -= 1;
+        }
+        if (H < 0) {
+            H = 24 + H;
+        }
+        System.out.print(H + M);
+    }
+}
