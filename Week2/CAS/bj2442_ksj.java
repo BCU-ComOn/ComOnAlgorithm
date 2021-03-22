6aa8a5d5bd40d090b759ea769a2f7f0d684a2dc0
pocachp <pocachip0318@gmail.com>
2021-03-22 22:44:08 +0900

    Create bj2442_ksj.java

---
+++ Week2/CAS/bj2442_ksj.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class bj2442_ksj {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for (int i = 0; i < num; i++) {
+            for (int j = 1; j < num - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
