7b099ca269671faf3083381ed04ca7ffc5e30342
LMY991 <dreamlmy1203@naver.com>
2021-03-29 17:45:16 +0900

    Create bj9086_lmy.java

---
+++ Week3/OME/bj9086_lmy.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class bj9086_lmy {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int T = scan.nextInt();
+		for(int i=0; i<T; i++) {
+			String str = scan.next();
+			System.out.println(str.charAt(0) + "" + str.charAt(str.length()-1));
+		}
+		
+		scan.close();
+	}
+
+}
