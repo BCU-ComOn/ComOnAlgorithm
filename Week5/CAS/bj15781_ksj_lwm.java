6d3b747c74b9e049bd75fecd08571b5c7f4ee579
woomin33 <woomin013@gmail.com>
2021-04-06 22:36:47 +0900

    [CAS-5]
    15781-헬멧과 조끼

---
+++ Week5/CAS/bj15781_ksj_lwm.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class bj15781_ksj_lwm {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int h = sc.nextInt();
+    int j = sc.nextInt();
+    int[] hArr = new int[h];
+    int[] jArr = new int[j];
+    int max = 0;
+
+    for (int i = 0; i < h; i++) {
+      hArr[i] = sc.nextInt();
+      if (max < hArr[i]) {
+        max = hArr[i];
+      }
+    }
+    h = max;
+    max = 0;
+    for (int k = 0; k < j; k++) {
+      jArr[k] = sc.nextInt();
+      if (max < jArr[k]) {
+        max = jArr[k];
+      }
+    }
+    j = max;
+    System.out.println(h + j);
+    sc.close();
+  }
+}
